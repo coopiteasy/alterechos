85492e219eeee0925d298c2b05feda86ebc57339
Rémy Taymans <remytaymans@gmail.com>
2019-09-18 16:01:24 +0200

    [ADD] alter_w_theme: New theme for alterechos
    Copyright to Alterechos.

---
+++ alter_website_theme/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Agence Alter Shop Theme',
+
+    'summary': """
+        Apply Agence Alter design rules.
+    """,
+    'description': """
+    """,
+
+    'author': 'Laurence/Ohmybox',
+    'website': "https://github.com/laurence001/alter",
+
+    'license': "AGPL-3",
+    'category': 'Themes',
+    'version': '1.0',
+
+    'depends': ['website'],
+
+    'data': [
+        'views/assets.xml',
+    ]
+}
