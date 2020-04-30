e82bdd499661fac229038b909ca7aff92d4dd075
robin.keunen <robin@keunen.net>
2020-04-30 18:17:32 +0200

    [ADD] alterechos_custom

---
+++ alterechos_custom/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Coop IT Easy SCRLfs
+#     Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Alter Echos",
+    "summary": """
+        Alter Echos custom module
+    """,
+    "description": """
+        Alter Echos custom module
+    """,
+    "author": "Coop IT Easy SCRLfs",
+    "website": "https://coopiteasy.be",
+    "license": "AGPL-3",
+    "category": "Website",
+    "version": "9.0.1.0.0",
+    "depends": [
+        "l10n_be",
+        "product_subscription",
+        "website_product_subscription",
+        "product_subscription_delivery",
+        "product_subscription_subscribers",
+        "account_journal_lock_date",
+        "account_archive_journals",
+    ],
+}
